328ca0d04ae174bd39f8bbadbe7092956c51195d
Alessandro Scandone <alescandone@gmail.com>
2021-12-12 19:56:01 +0100

    added test

---
+++ test/combinators/unknown.test.ts
@@ -0,0 +1,12 @@
+import { number, success, unknown, string, tuple } from "../../src/index";
+import { expectFail, expectSuccess } from "../utils";
+
+describe("Unknown", () => {
+  test("Should always succeed", () => {
+    expectSuccess(unknown, ["x", 42]);
+    expectSuccess(unknown, 0);
+    expectSuccess(unknown, true);
+    expectSuccess(unknown, null);
+    expectSuccess(unknown, undefined);
+  });
+});
