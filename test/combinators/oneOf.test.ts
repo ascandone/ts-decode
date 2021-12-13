aed4a18e1c5c4c01bc0c75c9693965fe2668d03a
Alessandro Scandone <alescandone@gmail.com>
2021-12-13 23:46:28 +0100

    edit type test

--- test/combinators/oneOf.test.ts
+++ test/combinators/oneOf.test.ts
@@ -1,24 +1,20 @@
-import { Infer, oneOf, string, undefined_ } from "../../src";
-import { assert, typeChecking, shouldFail, shouldPass } from "../TestHelpers";
+import { Decoder, Infer, oneOf, string, undefined_ } from "../../src";
+import { assert, typeChecking, shouldPass } from "../TestHelpers";
 import { expectSuccess } from "../utils";
 
 describe("oneOf", () => {
   test("primitives", () => {
     const dec = oneOf(string, undefined_);
 
-    type Z = Infer<typeof dec>;
-
-    type T = typeChecking<Infer<typeof dec>, string | undefined, shouldPass>;
-
     type Test1 = assert<
       [
         typeChecking<Infer<typeof dec>, string | undefined, shouldPass>,
-        // typeChecking<typeof dec, Decoder<string | undefined>, shouldPass>,
+        typeChecking<typeof dec, Decoder<string | undefined>, shouldPass>,
       ]
     >;
 
     expectSuccess(dec, "Hello");
-    // expectSuccess(dec, undefined);
+    expectSuccess(dec, undefined);
 
     expect(dec.decode(null).error).toBe(true);
   });
