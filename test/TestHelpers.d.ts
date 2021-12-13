a40111fe0b7b704bc0f6cf7b375677088eefdf59
Alessandro Scandone <alescandone@gmail.com>
2021-12-13 23:45:48 +0100

    fixed type assertion

--- test/TestHelpers.d.ts
+++ test/TestHelpers.d.ts
@@ -1,13 +1,17 @@
-export type Equals<X, Y> = X extends Y ? (Y extends X ? true : false) : false;
-
-type T = Equals<number, number>;
+export type Equals<X, Y> = [X] extends [Y]
+  ? [Y] extends [X]
+    ? true
+    : false
+  : false;
 
 export type shouldPass = true;
 export type shouldFail = false;
 
 export type assert<_ extends true[]> = void;
 
-export type typeChecking<T, U, Expect extends boolean> = Equals<
-  Equals<T, U>,
-  Expect
->;
+export type typeChecking<T, U, Expect extends true | false> = Equals<
+  T,
+  U
+> extends Expect
+  ? true
+  : false;
